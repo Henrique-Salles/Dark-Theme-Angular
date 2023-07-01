537002be2c7ba079ec70fb28b3e1e507ef8b1c4a
Henrique-Salles <henriquesalles1520@gmail.com>
2023-07-01 01:09:57 -0300

    Initial Commit

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ETheme } from '../../enums/ETheme.enum';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  public icon: string = ETheme.ICON_MOON;
+  public textTheme: string = ETheme.TEXT_MOON;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public toggle() {
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if (theme) {
+      this.icon = ETheme.TEXT_SUN;
+      return (this.icon = ETheme.ICON_SUN);
+    }
+    this.icon = ETheme.TEXT_MOON;
+    return (this.icon = ETheme.ICON_MOON);
+  }
+}
---
+++ src/app/core/enums/ETheme.enum.ts
@@ -0,0 +1,6 @@
+export enum ETheme {
+  ICON_MOON = 'moon',
+  ICON_SUN = 'sun',
+  TEXT_SUN = 'light theme',
+  TEXT_MOON = 'dark theme',
+}
